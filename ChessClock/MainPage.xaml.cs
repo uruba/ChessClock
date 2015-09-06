72b95848e6fce79f074c22ab51e44eb1bb8f7b36
Václav Uruba <uruba@outlook.com>
2015-09-06 20:39:11 +0200

    Initial commit

---
+++ ChessClock/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace ChessClock
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, TimeProviderListener
+    {
+        TimeProvider timeProviderPlayer1, timeProviderPlayer2;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            TimeSpan newTime = new TimeSpan(0,0,0);
+
+            this.timeProviderPlayer1 = new TimeProvider(this, buttonPlayer1, newTime);
+            this.timeProviderPlayer2 = new TimeProvider(this, buttonPlayer2, newTime);
+        }
+
+        public async void onTimeProviderTick(TimeSpan timeSpanCurrent, object sender)
+        {
+            Button button = (Button)sender;
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Low, () => {
+                button.Content = TimeProvider.formatTime(timeSpanCurrent.Hours, timeSpanCurrent.Minutes, timeSpanCurrent.Seconds);
+            });
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            buttonPlayer1.Content = "Player 1";
+            buttonPlayer2.Content = "Player 2";
+        }
+
+        private void buttonPlayer1_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            toggleTimer((Button)sender);
+        }
+
+        private void buttonPlayer2_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            toggleTimer((Button)sender);
+        }
+
+        private void toggleTimer(Button sender)
+        {
+            bool isTimerPlayer1Running = timeProviderPlayer1.isTimerRunning();
+            bool isTimerPlayer2Running = timeProviderPlayer2.isTimerRunning();
+
+            if (isTimerPlayer1Running || isTimerPlayer2Running)
+            {
+                if (
+                    (isTimerPlayer1Running && sender.Equals(buttonPlayer1)) 
+                    || 
+                    (isTimerPlayer2Running && sender.Equals(buttonPlayer2))
+                    ) {
+                    timeProviderPlayer1.toggleTimer();
+                    timeProviderPlayer2.toggleTimer();
+                }
+
+                return;
+            }
+            
+
+            if (sender.Equals(buttonPlayer1))
+            {
+                timeProviderPlayer1.startTimer();
+            }
+            else if (sender.Equals(buttonPlayer2))
+            {
+                timeProviderPlayer2.startTimer();
+            }
+        }
+    }
+}
---
+++ ChessClock/TimeProvider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ChessClock
+{
+    public sealed class TimeProvider
+    {
+        private TimeSpan timeSpanTarget;
+        private Stopwatch timer;
+
+        private readonly TimeProviderListener listener;
+        private readonly object sender;
+        private readonly TimerCallback timerCallback;
+        private readonly static int timerTickLength = 10;
+        private readonly TimeSpan timeSpanIncrement = new TimeSpan(0, 0, 0, 0, timerTickLength);
+        private readonly Timer cycleTimer;
+
+        public TimeProvider(TimeProviderListener listener, object sender)
+        {
+            this.timer = new Stopwatch();
+            this.listener = listener;
+            this.sender = sender;
+            timerCallback = new TimerCallback(timerTick);
+            cycleTimer = new Timer(this.timerTick, null, 0, 10);
+        }
+
+        public TimeProvider(TimeProviderListener listener, object sender, TimeSpan timeSpan) : this(listener, sender)
+        {
+            setTime(timeSpan);
+        }
+
+        public void setTime(TimeSpan timeSpan)
+        {
+            this.timeSpanTarget = timeSpan;
+        }
+
+        public void startTimer()
+        {
+            timer.Start();
+        }
+
+        public void stopTimer()
+        {
+            timer.Stop();
+        }
+
+        public void toggleTimer()
+        {
+            if (isTimerRunning())
+            {
+                stopTimer();
+            }
+            else
+            {
+                startTimer();
+            }
+        }
+
+        public void resetTimer()
+        {
+            timer.Reset();
+        }
+
+        public bool isTimerRunning()
+        {
+            return timer.IsRunning;
+        }
+
+        public static String formatTime(int hours, int minutes, int seconds)
+        {
+            return string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+        }
+
+        private void timerTick(object state)
+        {
+            listener.onTimeProviderTick(timer.Elapsed, sender);
+        }
+    }
+
+    public interface TimeProviderListener
+    {
+        void onTimeProviderTick(TimeSpan timeSpanCurrent, object sender);
+    }
+}
